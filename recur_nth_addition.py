44f215d028fe49a00bfe15c3d3ee9bd4c699934c
ksprivate <suraj.surajkolekar@gmail.com>
2023-04-05 16:00:06 +0530

    recursion

---
+++ recur_nth_addition.py
@@ -0,0 +1,12 @@
+# number = int(input("Enter nth number: "))
+
+# def recur_num(num):
+#     if num ==0:
+#         return 
+#     else:
+#         recur_num(num-1)
+#         print(num)
+
+# num_total = recur_num(number)
+# print(num_total)
+print(2>2)
